337c955a19ac271a3a7a0128a1463cc89e63614b
Steven Scott <stevescott2653@gmail.com>
2019-10-03 08:35:16 -0400

    Add files via upload

---
+++ Thread_Priority_Example/Thread_Priority_Example/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Thread_Priority_Example
+{
+    using System.Threading;
+
+    public class MyThread
+    {
+        public void Thread1()
+        {
+            Thread t = Thread.CurrentThread;
+            Console.WriteLine(t.Name+" is running");
+        }
+
+        public class ThreadExample
+        {
+            public static void Main()
+            {
+                MyThread mt = new MyThread();
+                Thread t1 = new Thread(new ThreadStart(mt.Thread1));
+                Thread t2 = new Thread(new ThreadStart(mt.Thread1));
+                Thread t3 = new Thread(new ThreadStart(mt.Thread1));
+                t1.Name = "Player1";
+                t2.Name = "Player2";
+                t3.Name = "Player3";
+                t3.Priority = ThreadPriority.Highest;
+                t2.Priority = ThreadPriority.Normal;
+                t1.Priority = ThreadPriority.Lowest;
+
+                t1.Start();
+                t2.Start();
+                t3.Start();
+            }
+        }
+    }
+}
